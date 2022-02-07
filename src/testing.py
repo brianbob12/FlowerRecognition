98b2d6495723f9f631812a71c2d0120d4dc43684
Cyrus Singer <japaneserhino@gmail.com>
2022-02-07 12:40:41 +0000

    Update testing.py

--- src/testing.py
+++ src/testing.py
@@ -7,7 +7,7 @@ from PIL import Image
 #STEP 1
 #setup learning manager and dataset
 tm=Elinvar.TM.TrainingManager()
-tm.setEpisodeEndRequirements(maxIterations=500)
+tm.setEpisodeEndRequirements(maxIterations=5000)
 
 #outputs x and y arrays
 def getBatch(fileNames):

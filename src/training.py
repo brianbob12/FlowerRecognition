65adfd3be5151d1570fe52984e7458e54debee09
Cyrus Singer <japaneserhino@gmail.com>
2022-05-03 11:12:24 +0100

    Update training.py

--- src/training.py
+++ src/training.py
@@ -65,9 +65,6 @@ def seriesX(name,learningRate):
   #NOTE: it is important to create a new CNN for each series
   #otherwise each training episode will continue with the same CNN
   #(unless that's what you want)
-  myErrorFunction=Elinvar.NN.ErrorFunctions.SoftmaxCrossEntropyWithLogits()
-  myTrainingProtocol=Elinvar.NN.XYTraining(learningRate,tf.keras.optimizers.Adam,[dense3],myErrorFunction)
-  te.trainingProtocol=myTrainingProtocol
 
   myNet=Elinvar.NN.Network()
 
@@ -120,6 +117,9 @@ def seriesX(name,learningRate):
   myNet.build()
 
   te.setNetwork(myNet)
+  myErrorFunction=Elinvar.NN.ErrorFunctions.SoftmaxCrossEntropyWithLogits()
+  myTrainingProtocol=Elinvar.NN.XYTraining(learningRate,tf.keras.optimizers.Adam,[dense3],myErrorFunction)
+  te.trainingProtocol=myTrainingProtocol
 
   #must be done after network imported
   #te.setUpWandB("flowerRecognition","japaneserhino")
